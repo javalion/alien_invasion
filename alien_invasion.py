d804946dd0301814a7eb18da7942b676d357bf80
Terry McKee <javalion@hotmail.com>
2025-10-06 07:40:22 -0500

    Add ship movement to the right capability

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,6 +25,7 @@ class AlienInvasion:
         print("Starting the game loop.")
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -33,6 +34,12 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Redraw the screen during each pass through the loop."""
